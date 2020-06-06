8159e12cbaa0922d04b65f301d3b2a800969b58f
Edgar Dias <edgarus@gmail.com>
2020-06-06 15:59:55 +0100

    add simplegraphics lib

--- src/ac/bootcamp/row1game/Main.java
+++ src/ac/bootcamp/row1game/Main.java
@@ -1,9 +1,24 @@
 package ac.bootcamp.row1game;
 
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Text;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Game");
+
+       Rectangle rectangle = new Rectangle(10,10, 300,300);
+
+        rectangle.setColor(Color.GREEN);
+        rectangle.fill();
+
+        Text text = new Text(15,15,"ROW1GAME");
+        text.setColor(Color.BLACK);
+        text.draw();
+
+
     }
 }
